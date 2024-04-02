f0c377c7d84b2a555fd6a3ed772309136c793deb
Ardalto <ardalto@gmail.com>
2024-04-02 19:00:30 -0300

    add style.css

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // egar a tag img
-  const img = document.querySelector("profile img")
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
 
   //substituir a imagem
-  if (html.classList.add.contains("light")) {
+  if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem
     img.setAttribute("src", "./assets/assets/avatar-light.png")
     text.setAtribute("alt", "Ardalto observando fixamente para frente")
   } else {
-    img.setAtribute("ssrc", "./assets/assets/Avatar.png")
+    img.setAtribute("src", "./assets/assets/Avatar.png")
   }
 }
